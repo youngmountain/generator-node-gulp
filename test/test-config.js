981466fb6027310aabf6892039a66c4ac70f3827
Stefan Buck <web@broud.de>
2014-04-24 23:54:21 +0200

    implement(test): Add config test

---
+++ test/test-config.js
@@ -0,0 +1,45 @@
+/*global describe, beforeEach, it*/
+'use strict';
+var assert = require('assert');
+var path = require('path');
+var helpers = require('yeoman-generator').test;
+var Config = require('../config');
+
+describe('generator config', function () {
+
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) {
+        return done(err);
+      }
+      done();
+    }.bind(this));
+  });
+
+  it('wirte meta infos', function () {
+
+    var config = new Config('config.json');
+    config.setMeta({
+      githubUsername: 'tom',
+      authorName: 'Tom Jerry',
+      authorEmail: 'tom@jerry.org',
+      authorUrl: 'http://jerry.org'
+    });
+
+    var expectedFiles = [
+      'config.json'
+    ];
+
+    var expectedContent = [
+      ['config.json',
+        /"githubUsername": "tom"/,
+        /"authorName": "Tom Jerry"/,
+        /"authorEmail": "tom@jerry.org"/,
+        /"authorUrl": "http:\/\/jerry.org"/
+      ]
+    ];
+
+    helpers.assertFile(expectedFiles);
+    helpers.assertFileContent(expectedContent);
+  });
+});
