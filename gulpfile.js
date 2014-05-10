01e2671c78b20dc0c20d1512eba10241b7b91b16
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-05-10 08:56:20 +0200

    add gulp watch task for tests

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,11 @@ var jscs = require('gulp-jscs');
 var istanbul = require('gulp-istanbul');
 var mocha  = require('gulp-mocha');
 var bump   = require('gulp-bump');
+var _ = require('lodash');
 
 var paths = {
   lint: ['./gulpfile.js', './app/index.js', './config.js'],
-  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
+  tests: ['./test/**/*.js', '!./test/temp/**/*.js'],
   source: ['./lib/*.js', './app/index.js', './config.js']
 };
 
@@ -24,7 +25,7 @@ gulp.task('istanbul', function (cb) {
   gulp.src(paths.source)
     .pipe(istanbul()) // Covering files
     .on('end', function () {
-      gulp.src(paths.tests)
+      gulp.src(paths.tests, {cwd: __dirname})
         .pipe(mocha())
         .pipe(istanbul.writeReports()) // Creating the reports after tests runned
         .on('end', cb);
@@ -37,6 +38,11 @@ gulp.task('bump', ['test'], function () {
   return gulp.src(['./package.json'])
     .pipe(bump({ type: bumpType }))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+  gulp.run('istanbul');
+  gulp.watch(_.union(paths.lint, paths.tests), ['istanbul']);
 });
 
 gulp.task('test', ['lint', 'istanbul']);
