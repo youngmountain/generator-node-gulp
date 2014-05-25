61a8fec1af841a90d5c288b446b5a08124d733d9
stefanbuck <web@broud.de>
2014-05-25 10:59:30 +0200

    Use single line comments instead of multi-line comments

--- app/templates/lib/name.js
+++ app/templates/lib/name.js
@@ -8,19 +8,17 @@
 
 'use strict';
 
-/**
- * Following the 'Node.js require(s) best practices' by
- * http://www.mircozeiss.com/node-js-require-s-best-practices/
- *
- * // Nodejs libs
- * var fs = require('fs'),
- *
- * // External libs
- * debug = require('debug'),
- *
- * // Internal libs
- * data = require('./data.js');
- */
+// Following the 'Node.js require(s) best practices' by
+// http://www.mircozeiss.com/node-js-require-s-best-practices/
+
+// // Nodejs libs
+// var fs = require('fs'),
+//
+// // External libs
+// debug = require('debug'),
+//
+// // Internal libs
+// data = require('./data.js');
 
 exports.awesome = function() {
   return 'awesome';
